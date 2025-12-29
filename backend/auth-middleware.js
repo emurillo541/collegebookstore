2268c34b5fc52d7e754e539fe1a1204d12ab17fd
Frosty <murillem@oregonstate.edu>
2025-12-28 19:34:36 -0800

    Update auth-middleware.js

--- backend/auth-middleware.js
+++ backend/auth-middleware.js
@@ -1,11 +1,8 @@
 const { auth } = require('express-oauth2-jwt-bearer');
 
 const jwtCheck = auth({
-    
-    issuerBaseURL: process.env.AUTH0_ISSUER,
-    
-    
-    audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
+  audience: process.env.AUTH0_AUDIENCE,
 });
 
 module.exports = { jwtCheck };
